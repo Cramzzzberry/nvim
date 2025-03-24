ddfcc661e9a4b91137ead3521e9ae8b240a424aa
Jan Roe Bantuan <cramzzzberry@gmail.com>
2025-03-25 06:30:29 +0800

    feat: changed the split position of Fugitive Git status

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-fugitive',
   config = function()
     -- Open Fugitive Status
-    vim.keymap.set("n", "<leader>gs", ":vertical G<cr>")
+    vim.keymap.set("n", "<leader>gs", ":topleft vertical G<cr>")
 
     -- Open diff in 3 vertical splits
     vim.keymap.set("n", "<leader>gvds", ":Gvdiffsplit!<cr>")
