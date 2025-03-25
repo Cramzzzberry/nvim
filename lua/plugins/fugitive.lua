328c39b836edcbe43afeac97e5fd1114827bfdcc
Jan Roe Bantuan <cramzzzberry@gmail.com>
2025-03-25 19:29:17 +0800

    feat: added more keymaps for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,9 +2,18 @@ return {
   'tpope/vim-fugitive',
   config = function()
     -- Open Fugitive Status
-    vim.keymap.set("n", "<leader>gs", ":topleft vertical G<cr>")
+    vim.keymap.set("n", "<leader>gs", ":topleft vertical G<cr>", { silent = true })
+
+    -- Open Git Log in split view
+    vim.keymap.set("n", "<leader>gl", ":topleft vertical Git log<cr>", { silent = true })
+
+    -- Open Git blame
+    vim.keymap.set("n", "<leader>gbl", ":Git blame<cr>", { silent = true })
+
+    -- List branches
+    vim.keymap.set("n", "<leader>gbr", ":Git branch")
 
     -- Open diff in 3 vertical splits
-    vim.keymap.set("n", "<leader>gvds", ":Gvdiffsplit!<cr>")
+    vim.keymap.set("n", "<leader>gvds", ":Gvdiffsplit!<cr>", { silent = true })
   end
 }
