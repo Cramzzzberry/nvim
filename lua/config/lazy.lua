5be4fb9192ebe9037e0b061ce324d84c0ec501b7
Jan Roe Bantuan <cramzzzberry@gmail.com>
2025-03-26 17:33:06 +0800

    refactor: removed duplicate statements

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,26 +15,15 @@ local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stab
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Set the size of the tabs and make it into spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
 -- Setup lazy.nvim
 require("lazy").setup({
-spec = {
--- import your plugins
-{ import = "plugins" },
-},
--- Configure any other settings here. See the documentation for more details.
--- colorscheme that will be used when installing plugins.
-install = { colorscheme = { "habamax" } },
--- automatically check for plugin updates
-checker = { enabled = true },
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })
